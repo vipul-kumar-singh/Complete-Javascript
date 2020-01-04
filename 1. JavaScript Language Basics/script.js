89e885b8d889ff30c2fc61ad85161e32b5a331ec
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-01-05 00:53:13 +0530

    Data types and variables

--- 1. JavaScript Language Basics/script.js
+++ 1. JavaScript Language Basics/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World!!');
+//String datatype
+var firstName = 'John';
+var lastName = 'Smith';
+console.log(firstName);
+
+//Numeric datatype
+var age = 28;
+console.log(age);
+
+//boolean datatype
+var fullAge = true;
+console.log(fullAge);
+
+//undefined datatype
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//varible names cannot start with a number
+//but '$' and '_' are allowed
+//and these two are the only special characters allowed in variable name
+var $3years = 3;
+var _isAdult = false;
+
+
+//resersed keywords like 'function', 'delete', 'var' are also not allowed
