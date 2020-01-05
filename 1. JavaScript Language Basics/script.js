e4db94f214aa8ea9e6cf107bf9a9fb136b2392f4
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-01-05 15:03:03 +0530

    Basic Operators

--- 1. JavaScript Language Basics/script.js
+++ 1. JavaScript Language Basics/script.js
@@ -38,6 +38,7 @@ var _isAdult = false;
  * Variable Mutation and Type Coercion
  */
 
+ /*
  var firstName = 'John'
  var age = 28;
 
@@ -60,3 +61,41 @@ var _isAdult = false;
  var middleName = prompt("What is his middle name?");
  var lastName = prompt("What is his last name?", _default='Smith');
  console.log(firstName + ' '+ middleName +' ' + lastName);
+ */
+
+
+
+/**********************************************************************************************
+ * Basic operators
+ */
+
+var year, yearJohn, yearMark;
+now = 2010;
+ageJohn = 28;
+ageMark = 33;
+
+
+//Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+ 
+//Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+
