35829680166944f3e17fe71de435c005acf913a6
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-01-19 22:52:39 +0530

    Coding Challenge #5

--- 1. JavaScript Language Basics/Coding Challenges/script.js
+++ 1. JavaScript Language Basics/Coding Challenges/script.js
@@ -119,7 +119,7 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 
 GOOD LUCK 😀
 */
-
+/*
 var john = {
     fullName: 'John Smith',
     mass: 110,
@@ -147,3 +147,93 @@ if (john.calcBMI() > mark.calcBMI()) {
 } else {
     console.log('They have the same BMI');
 }
+*/
+
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+var john = {
+    bills : [124,48,180,42],
+    tips : [],
+    finalAmount : [],
+    calculateTip : function(bills, tips, finalAmount){
+        for(var i = 0; i < bills.length; i++){
+            if(bills[i] < 50)
+                tips[i] = 0.2 * bills[i];
+            else if(bills[i] >= 50 && bills[i] <= 200)
+                tips[i] = 0.15 * bills[i];
+            else
+                tips[i] = 0.1 * bills[i];
+
+            finalAmount[i] = bills[i] + tips[i];
+        }
+    }
+}
+john.calculateTip(john.bills, john.tips, john.finalAmount);
+
+var mark = {
+    bills : [77,375,110,45],
+    tips : [],
+    finalAmount : [],
+    calculateTip : function(bills, tips, finalAmount){
+        for(var i = 0; i < bills.length; i++){
+            if(bills[i] < 100)
+                tips[i] = 0.2 * bills[i];
+            else if(bills[i] >= 100 && bills[i] <= 300)
+                tips[i] = 0.1 * bills[i];
+            else
+                tips[i] = 0.25 * bills[i];
+
+            finalAmount[i] = bills[i] + tips[i];
+        }
+    }
+}
+mark.calculateTip(mark.bills, mark.tips, mark.finalAmount);
+
+function compareAvg(tipsJohn, tipsMark) {
+    var johnAvg = calcAvg(tipsJohn);
+    var markAvg = calcAvg(tipsMark);
+
+    if (johnAvg > markAvg)
+        console.log('John\'s family paid the highest tips.');
+    else if (johnAvg < markAvg)
+        console.log('Mark\'s family paid the highest tips.');
+    else
+        console.log('John and Mark paid equal tips.');
+        
+}
+
+function calcAvg(tips){
+    var sum = 0;
+    for(var i = 0; i < tips.length; i++)
+        sum += tips[i];
+    return sum/tips.length;
+}
+
+console.log(compareAvg(john.tips, mark.tips));
