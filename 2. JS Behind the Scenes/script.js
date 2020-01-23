a0913dc985016920fc89d51abffcb2e366b5eef7
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-01-23 22:45:49 +0530

    Scoping

--- 2. JS Behind the Scenes/script.js
+++ 2. JS Behind the Scenes/script.js
@@ -1,7 +1,7 @@
 /********************************
 *  Lecture: Hoisting
 */
-
+/*
 //functions
 calculateAge(1965);
 
@@ -27,7 +27,7 @@ function foo() {
 }
 foo();
 console.log(age);
-
+*/
 
 
 /********************************
@@ -36,7 +36,7 @@ console.log(age);
 
 // First scoping example
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -49,13 +49,13 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -73,7 +73,7 @@ function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
-*/
+
 
 
 
