37f20e9cf1dd0b924988858f8fd16ca2c63180fa
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-01-26 00:52:07 +0530

    Updating Scores and Changing the Active Player

--- 3. DOM - Pig Game/app.js
+++ 3. DOM - Pig Game/app.js
@@ -41,5 +41,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score only if the round score is NOT 1, otherwise set round score to zero
+    if (dice !== 1) {
+        //Add Score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else{
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
